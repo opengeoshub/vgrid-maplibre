be56a478f85517e1cff34f8b3c88208d52186b19
thangqd <quachdongthang@gmail.com>
2024-12-01 15:25:45 +0700

    update H3Grid

--- H3/H3Grid.js
+++ H3/H3Grid.js
@@ -1,14 +1,51 @@
 class H3Grid {
-  constructor(map) {
+  constructor(map, options = {}) {
     this.map = map;
     this.latitudeMax = 90;
     this.latitudeMin = -this.latitudeMax;
     this.longitudeMax = 180;
     this.longitudeMin = -this.longitudeMax;
     this.extraFillArea = 0.5;
-    this.hexLayer = 'hex-layer';
-    this.hexSource = 'hex-source';
-    this.hexLabelLayer = 'hex-label';
+    this.map = map;
+    this.options = {
+      // color: options.color || 'rgba(255, 0, 0, 1)',
+      redraw: options.redraw || 'move', // Default to redraw on move
+    };
+    this.sourceId = 'h3-grid';
+    this.gridLayerId = 'h3-grid-layer';
+    this.labelLayerId = 'h3-label-layer';
+    this.initialize();
+  }
+
+  initialize() {
+    // Add a GeoJSON source for the grid
+    this.map.addSource(this.sourceId, {
+      type: 'geojson',
+      data: this.generateGrid(),
+    });
+
+    this.map.addLayer({
+      id: this.gridLayerId,
+      source: this.sourceId,
+      type: 'fill',
+      layout: {},
+      paint: {
+        'fill-color': 'transparent',
+        'fill-opacity': 1,
+        'fill-outline-color': ['get', 'color'],
+      }
+    });
+
+    // Redraw the grid on map movements
+    this.map.on(this.options.redraw, () => this.updateGrid());
+  }
+
+  updateGrid() {
+    const newGrid = this.generateGrid();
+    const source = this.map.getSource(this.sourceId);
+    if (source) {
+      source.setData(newGrid);
+    }
   }
 
   // Determine the H3 resolution based on zoom level
@@ -32,38 +69,46 @@ class H3Grid {
   }
 
   // Render hexagons based on current map zoom
-  renderHexes() {
-    const currentZoom = this.map.getZoom();
-    const h3res = this.getResolution(currentZoom);
-
+  generateGrid() {
+    const latitudeMax = 90;
+    const latitudeMin = -latitudeMax;
+    const longitudeMax = 180;
+    const longitudeMin = -longitudeMax;
+  
+    const extraFillArea = 0.5;
+    const hexLayer = 'hex-layer';
+    const hexSource = 'hex-source';
+    const hexlabelLayer = 'hex-label';
+  
+    var currentZoom = this.map.getZoom();
+    var h3res = getResolution(currentZoom);
+  
     const iw = window.innerWidth;
     const ih = window.innerHeight;
     const cUL = this.map.unproject([0, 0]).toArray(); // Upper left
     const cLR = this.map.unproject([iw, ih]).toArray(); // Lower right
-
-    let x1 = Math.min(cUL[0], cLR[0]);
-    let x2 = Math.max(cUL[0], cLR[0]);
-    let y1 = Math.min(cUL[1], cLR[1]);
-    let y2 = Math.max(cUL[1], cLR[1]);
-
-    let dh = x2 - x1;
-    let dv = y2 - y1;
-
-    let x1withBuffer = x1 - dh * this.extraFillArea;
-    let x2withBuffer = x2 + dh * this.extraFillArea;
-    let y1withBuffer = y1 - dv * this.extraFillArea;
-    let y2withBuffer = y2 + dv * this.extraFillArea;
-
-    x1withBuffer = Math.max(x1withBuffer, this.longitudeMin);
-    x2withBuffer = Math.min(x2withBuffer, this.longitudeMax);
-    y1withBuffer = Math.max(y1withBuffer, this.latitudeMin);
-    y2withBuffer = Math.min(y2withBuffer, this.latitudeMax);
-
+    const x1 = Math.min(cUL[0], cLR[0]);
+    const x2 = Math.max(cUL[0], cLR[0]);
+    const y1 = Math.min(cUL[1], cLR[1]);
+    const y2 = Math.max(cUL[1], cLR[1]);
+    const dh = x2 - x1;
+    const dv = y2 - y1;
+  
+    let x1withBuffer = x1 - dh * extraFillArea;
+    let x2withBuffer = x2 + dh * extraFillArea;
+    let y1withBuffer = y1 - dv * extraFillArea;
+    let y2withBuffer = y2 + dv * extraFillArea;
+  
+    x1withBuffer = Math.max(x1withBuffer, longitudeMin);
+    x2withBuffer = Math.min(x2withBuffer, longitudeMax);
+    y1withBuffer = Math.max(y1withBuffer, latitudeMin);
+    y2withBuffer = Math.min(y2withBuffer, latitudeMax);
+  
     let coordinates = [];
     const xIncrement = 180;
     let lowerX = x1withBuffer;
-
-    while (lowerX < this.longitudeMax && lowerX < x2withBuffer) {
+  
+    while (lowerX < longitudeMax && lowerX < x2withBuffer) {
       let upperX = Math.min(lowerX + xIncrement, x2withBuffer, 180);
       coordinates.push([
         [y2withBuffer, lowerX],
@@ -73,86 +118,65 @@ class H3Grid {
       ]);
       lowerX += xIncrement;
     }
-
-    const shapes = [].concat(...coordinates.map(e => h3.polygonToCells(e, h3res)));
-    const features = shapes.map(cellId => this.createFeature(cellId, h3res));
-
-    const featureCollection = {
+  
+    var shapes = [].concat(...coordinates.map(e => h3.polygonToCells(e, h3res)));
+    var features = [];
+  
+    for (var i = 0; i < shapes.length; i++) {
+      let cellId = shapes[i];
+      let boundary = h3.cellToBoundary(cellId, true);
+  
+      // Adjust boundary coordinates if they cross the anti-meridian
+      if (boundary.find((e) => e[0] < -128) !== undefined) {
+        boundary = boundary.map((e) => e[0] > 0 ? [e[0] - 360, e[1]] : e);
+      }
+      const precision = h3.getResolution(cellId);
+      const edge_unit = precision > 7 ? h3.UNITS.m : h3.UNITS.km;
+      const area_unit = precision > 7 ? h3.UNITS.m2 : h3.UNITS.km2;
+  
+  
+      const icosa_faces = h3.getIcosahedronFaces(cellId);
+  
+      // const edge_len = h3.edgeLength(cellId,edge_unit);
+      let  avg_edge_len = h3.getHexagonEdgeLengthAvg(h3res, edge_unit);
+      avg_edge_len = parseFloat(avg_edge_len.toFixed(1)).toLocaleString();
+  
+      let  area = h3.cellArea(cellId, area_unit);
+      area = parseFloat(area.toFixed(1)).toLocaleString();
+  
+      let  avg_area = h3.getHexagonAreaAvg(h3res, area_unit);
+      avg_area = parseFloat(avg_area.toFixed(1)).toLocaleString();
+  
+      let  num_hex = h3.getNumCells(h3res);
+      num_hex = num_hex.toLocaleString();
+  
+      features.push({
+        "type": "Feature",
+        "properties": {
+          "color": h3.isPentagon(cellId) ? 'red' : 'blue',
+          "cellId": cellId, // Include the cell ID as a property
+          "precision": precision,
+          "icosa_faces": icosa_faces,
+          "area": area,
+          "avg_area": avg_area,
+          "area_unit": area_unit,
+          // "edge_len": edge_len,
+          "avg_edge_len": avg_edge_len,
+          "edge_unit": edge_unit,
+          "num_hex": num_hex
+        },
+        "geometry": {
+          "type": "Polygon", // Ensure this is a polygon
+          "coordinates": [boundary] // Set the boundary as the polygon's coordinates
+        }
+      });
+    }
+    const geojson_features = {
       type: "FeatureCollection",
-      features: features
+      features,
     };
-
-    this.updateMap(featureCollection);
-  }
-
-  // Create a feature for each cell
-  createFeature(cellId, h3res) {
-    let boundary = h3.cellToBoundary(cellId, true);
-
-    // Adjust boundary coordinates if they cross the anti-meridian
-    if (boundary.find(e => e[0] < -128) !== undefined) {
-      boundary = boundary.map(e => e[0] > 0 ? [e[0] - 360, e[1]] : e);
-    }
-
-    const precision = h3.getResolution(cellId);
-    const edgeUnit = precision > 7 ? h3.UNITS.m : h3.UNITS.km;
-    const areaUnit = precision > 7 ? h3.UNITS.m2 : h3.UNITS.km2;
-
-    const icosaFaces = h3.getIcosahedronFaces(cellId);
-    const avgEdgeLen = parseFloat(h3.getHexagonEdgeLengthAvg(h3res, edgeUnit).toFixed(1)).toLocaleString();
-    let area = parseFloat(h3.cellArea(cellId, areaUnit).toFixed(1)).toLocaleString();
-    const avgArea = parseFloat(h3.getHexagonAreaAvg(h3res, areaUnit).toFixed(1)).toLocaleString();
-    const numHex = h3.getNumCells(h3res).toLocaleString();
-
-    return {
-      type: "Feature",
-      properties: {
-        color: h3.isPentagon(cellId) ? 'red' : 'blue',
-        cellId,
-        precision,
-        icosaFaces,
-        area,
-        avgArea,
-        areaUnit,
-        avgEdgeLen,
-        edgeUnit,
-        numHex
-      },
-      geometry: {
-        type: "Polygon",
-        coordinates: [boundary]
-      }
-    };
-  }
-
-  // Update the map with the feature collection
-  updateMap(featureCollection) {
-    const hexSource = this.map.getSource(this.hexSource);
-    if (hexSource !== undefined) {
-      hexSource.setData(featureCollection);
-    } else {
-      const hexGeoJson = {
-        type: 'geojson',
-        data: featureCollection,
-      };
-      this.map.addSource(this.hexSource, hexGeoJson);
-      this.addLayerToMap();
-    }
-  }
-
-  // Add the hexagon layer to the map
-  addLayerToMap() {
-    this.map.addLayer({
-      id: this.hexLayer,
-      source: this.hexSource,
-      type: 'fill',
-      layout: {},
-      paint: {
-        'fill-color': 'transparent',
-        'fill-opacity': 1,
-        'fill-outline-color': ['get', 'color'],
-      }
-    });
+    // console.log(geojson_features)
+    return geojson_features
   }
 }
 export default H3Grid;
